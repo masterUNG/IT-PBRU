34def4b68583c79c20decfab563c2d93fde6d7f0
masterUNG <phrombutr@gmail.com>
2016-06-07 11:02:07 +0700

    Check Space Finish

---
+++ app/src/main/java/appewtc/masterung/itpbru/MyAlert.java
@@ -0,0 +1,31 @@
+package appewtc.masterung.itpbru;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
+/**
+ * Created by masterUNG on 6/7/16 AD.
+ */
+public class MyAlert {
+
+    public void myDialog(Context context,
+                         String strTitle,
+                         String strMessage) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setCancelable(false);
+        builder.setIcon(R.drawable.doremon48);
+        builder.setTitle(strTitle);
+        builder.setMessage(strMessage);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        builder.show();
+
+    }   // myDialog
+
+}   // Main Class
