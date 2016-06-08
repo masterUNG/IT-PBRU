d79e66fe178e75d515c803ff2610b19f379878ee
masterUNG <phrombutr@gmail.com>
2016-06-08 16:04:25 +0700

    Upload Value

--- app/src/main/java/appewtc/masterung/itpbru/CalendarActivity.java
+++ app/src/main/java/appewtc/masterung/itpbru/CalendarActivity.java
@@ -32,7 +32,7 @@ public class CalendarActivity extends AppCompatActivity {
 
     private void confirmDialog(int day, int month, int year) {
 
-        String strDate = Integer.toString(day) +
+        final String strDate = Integer.toString(day) +
                 "/" + Integer.toString(month + 1) +
                 "/" + Integer.toString(year);
 
@@ -54,6 +54,7 @@ public class CalendarActivity extends AppCompatActivity {
                 Intent intent = new Intent(CalendarActivity.this, UploadAccount.class);
                 intent.putExtra("Login", getIntent().getStringArrayExtra("Login"));
                 intent.putExtra("InOut", 0);
+                intent.putExtra("Date", strDate);
                 startActivity(intent);
 
                 dialogInterface.dismiss();
@@ -66,6 +67,7 @@ public class CalendarActivity extends AppCompatActivity {
                 Intent intent = new Intent(CalendarActivity.this, UploadAccount.class);
                 intent.putExtra("Login", getIntent().getStringArrayExtra("Login"));
                 intent.putExtra("InOut", 1);
+                intent.putExtra("Date", strDate);
                 startActivity(intent);
 
                 dialogInterface.dismiss();
