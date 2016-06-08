5f3c66784ebe3d1f5eaec39aab6ea99bbea8976b
masterUNG <phrombutr@gmail.com>
2016-06-08 13:29:30 +0700

    Last

--- app/src/main/java/appewtc/masterung/itpbru/CalendarActivity.java
+++ app/src/main/java/appewtc/masterung/itpbru/CalendarActivity.java
@@ -2,6 +2,7 @@ package appewtc.masterung.itpbru;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.CalendarView;
@@ -49,6 +50,9 @@ public class CalendarActivity extends AppCompatActivity {
         builder.setPositiveButton("รายรับ", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
+
+                Intent intent = new Intent(CalendarActivity.this, UploadAccount.class);
+
                 dialogInterface.dismiss();
             }
         });
---
+++ app/src/main/java/appewtc/masterung/itpbru/UploadAccount.java
@@ -0,0 +1,13 @@
+package appewtc.masterung.itpbru;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UploadAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_account);
+    }
+}
